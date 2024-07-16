5e320234d2ea9ebd940dfec3b7df9e302c536fb2
axartmiami@gmail.com <axartmiami@gmail.com>
2024-07-16 12:45:46 -0400

    created user repo

---
+++ src/main/java/com/expense/tracker/repository/ExpensesRepository.java
@@ -0,0 +1,4 @@
+package com.expense.tracker.repository;
+
+public interface ExpensesRepository {
+}
---
+++ src/main/java/com/expense/tracker/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.expense.tracker.repository;
+
+import com.expense.tracker.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
