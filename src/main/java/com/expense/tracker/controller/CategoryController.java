211d3a35b649d259f5cb39b9c6f9f9cf467b8ea6
axartmiami@gmail.com <axartmiami@gmail.com>
2024-07-17 14:10:50 -0400

    added category controller

--- src/main/java/com/expense/tracker/controller/CategoryController.java
+++ src/main/java/com/expense/tracker/controller/CategoryController.java
@@ -7,10 +7,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping
+    public List<Category> getAllCategories(@RequestParam Long userId) {
+        return categoryRepository.findByUserId(userId);
+    }
+
+    @PostMapping
+    public Category addCategory(@RequestBody Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @PutMapping("/{id}")
+    public Category updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        category.setId();
+        return categoryRepository.save(category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id) {
+        categoryRepository.deleteById(id);
+    }
 }
